30855d8d946f8208efba6607c6f094939ce5de97
purnachandraobulasetty <purnachandra.tech@gmail.com>
2024-07-20 16:45:52 +0530

    SwiftUI essentials
    SwiftUI essentials
    
    https://developer.apple.com/tutorials/swiftui/creating-and-combining-views

---
+++ SwiftUI/ListsAndNavigation/ListsAndNavigation/ListsAndNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListsAndNavigationApp.swift
+//  ListsAndNavigation
+//
+//  Created by Purnachandra on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ListsAndNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
